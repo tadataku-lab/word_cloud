5e2002234093487df337e9264a8e4e39aea7fd9b
tadataku-lab <tadataku.lab@gmail.com>
2019-06-09 16:40:14 +0900

    ref: divide for parts per functions

--- get_tweets.py
+++ get_tweets.py
@@ -8,16 +8,28 @@ AT = config.ACCESS_TOKEN
 ATS = config.ACCESS_TOKEN_SECRET
 twitter = OAuth1Session(CK, CS, AT, ATS)
 
-url = "https://api.twitter.com/1.1/statuses/user_timeline.json" #タイムライン取得エンドポイント
+TL_URL = "https://api.twitter.com/1.1/statuses/user_timeline.json"
 
-params ={'count' : 5} #取得数
-res = twitter.get(url, params = params)
+class GetTweetsError(Exception):
+  """Exception raised for get tweets.
 
-if res.status_code == 200: #正常通信出来た場合
-    timelines = json.loads(res.text) #レスポンスからタイムラインリストを取得
-    for line in timelines: #タイムラインリストをループ処理
-        print(line['user']['name']+'::'+line['text'])
-        print(line['created_at'])
-        print('*******************************************')
-else: #正常通信出来なかった場合
-    print("Failed: %d" % res.status_code)
+  Attributes:
+      status_code -- a returned status code
+  """
+
+  def __init__(self, status_code):
+    self.message = f'Getting tweets is failure! Status code is {status_code}.'
+    self.status_code = status_code
+
+def get_tweets(**params) -> list:
+  res = twitter.get(TL_URL, params = params)
+  if res.status_code == 200:
+    return json.loads(res.text)
+  else:
+    raise GetTweetsError(res.status_code)
+
+def dump_tweets(**params):
+  for line in get_tweets(**params):
+    print(line['user']['name']+'::'+line['text'])
+    print(line['created_at'])
+    print('*******************************************')
